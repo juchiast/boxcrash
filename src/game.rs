25bf1ff86ab4f382c12691f2a85fc2320389e38d
Do Duy <juchiast@gmail.com>
2017-02-22 20:23:13 +0700

    Add some more configs

--- src/game.rs
+++ src/game.rs
@@ -11,17 +11,20 @@ pub struct Game {
 }
 
 pub struct GameConfig {
+    pub title: &'static str,
     pub screen_size: Pixel,
+    pub ups: u64,
+    pub max_fps: u64,
 }
 
 impl Game {
     pub fn new(config: GameConfig) -> Game {
         let opengl = OpenGL::V3_2;
         let mut window: PistonWindow = WindowSettings::new(
-            "Box crash", [config.screen_size.w, config.screen_size.h])
+            config.title, [config.screen_size.w, config.screen_size.h])
             .opengl(opengl).exit_on_esc(true).build().unwrap();
-        window.set_ups(60);
-        window.set_max_fps(60);
+        window.set_ups(config.ups);
+        window.set_max_fps(config.max_fps);
         let world = World::new();
         Game {
             config: config,
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@ use cgmath::Vector3;
 
 fn main() {
     let config = GameConfig {
+        title: "Box Crash",
         screen_size: Pixel::new(800, 600),
+        ups: 60,
+        max_fps: 60,
     };
     Game::new(config).run();
 }
